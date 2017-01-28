0c002813bdaa22a1f0bf53950c518a32a1cf62ef
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-28 08:22:08 +0000

    add ex5.rb

--- methods_sorting/exercises/ex4.rb
+++ methods_sorting/exercises/ex4.rb
@@ -8,6 +8,8 @@ end
 # result = {'a' => 'ant', 'b' => 'bear', 'c' => 'cat'}
 
 =begin
+
+observation
 for the first iteration
 value = 'ant'
 hash = {}
@@ -17,4 +19,8 @@ hash['a'] = 'ant' # => { 'a' => 'ant' }
 
 repeat the above process for all the iteration untill there is no more
 append the result into the hash.
+
+notes
+the each_with_object takes an object as an argument and passes it through a block of code and returns a new array which is determined by the block of code
+
 =end
---
+++ methods_sorting/exercises/ex5.rb
@@ -0,0 +1,8 @@
+# ex5.rb
+
+# what does shift do in the following code?
+hash = { a: 'ant', b: 'bear' }
+hash.shift
+
+# here shift removes the first key/value pair and then returns an array with key and value as its elements in order.
+# Answer = [:a, 'ant']
