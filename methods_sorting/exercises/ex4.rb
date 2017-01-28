381e2b81458c75e529e06667e40c668c87606e07
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-28 08:08:56 +0000

    add

---
+++ methods_sorting/exercises/ex4.rb
@@ -0,0 +1,20 @@
+# ex4.rb
+
+# what is the return of each_with_object?
+['ant', 'bear', 'cat'].each_with_object({}) do |value, hash|
+  hash[value[0]] = value
+end
+
+# result = {'a' => 'ant', 'b' => 'bear', 'c' => 'cat'}
+
+=begin
+for the first iteration
+value = 'ant'
+hash = {}
+value[0] = 'a' # => ie the string present at the index 0
+hash[value[0]] = value
+hash['a'] = 'ant' # => { 'a' => 'ant' }
+
+repeat the above process for all the iteration untill there is no more
+append the result into the hash.
+=end
