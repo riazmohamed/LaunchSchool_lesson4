4903e6fe3c4e7abcdc0a9dac5486de02fc1e1ad8
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-31 06:45:23 +0000

    more info

--- working_with_blocks/exercises/ex7.rb
+++ working_with_blocks/exercises/ex7.rb
@@ -2,6 +2,7 @@
 
 arr = [[2], [3, 5, 7], [9], [11, 13, 15]]
 
+puts '-----------My solution------------'
 multiples_of_three = arr.map do |sub_arr|
       sub_arr.select do |obj|
         obj % 3 == 0
@@ -9,3 +10,19 @@ multiples_of_three = arr.map do |sub_arr|
     end
 
 p multiples_of_three
+
+puts '----------Given solution-------------'
+arr.map do |element|
+  element.select do |num|
+    num % 3 == 0
+  end
+end
+# => [[], [3], [9], [15]]
+
+puts '----------Given solution-------------'
+arr.map do |element|
+  element.reject do |num|
+    num % 3 != 0
+  end
+end
+# => [[], [3], [9], [15]]
