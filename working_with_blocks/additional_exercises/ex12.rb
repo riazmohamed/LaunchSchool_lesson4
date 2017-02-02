9e37750a78e0451cb2212979ec43e054aacd1765
Riaz Mohamed <railsdevriaz@gmail.com>
2017-02-02 08:33:18 +0000

    add ex12.rb

---
+++ working_with_blocks/additional_exercises/ex12.rb
@@ -0,0 +1,14 @@
+# ex12.rb
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+# my solution
+munsters.each do |name, details|
+  puts "#{name} is #{details["age"]} year old #{details["gender"]}."
+end
