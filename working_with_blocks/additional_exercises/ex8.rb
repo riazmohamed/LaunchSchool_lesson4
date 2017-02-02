22b3e20977a6868c1affd9fb34eb9daf6ef88269
Riaz Mohamed <railsdevriaz@gmail.com>
2017-02-02 06:25:33 +0000

    add ex8.rb

---
+++ working_with_blocks/additional_exercises/ex8.rb
@@ -0,0 +1,11 @@
+# ex8.rb
+
+words = "the flintstones rock"
+# my solution
+new_arr = words.split(" ").each { |word| word.capitalize!}.join(" ")
+words = new_arr
+p words
+
+# given solution
+words.split.map { |word| word.capitalize }.join(' ')
+p words
